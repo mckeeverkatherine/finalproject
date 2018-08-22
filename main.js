a2687b1ceeae761654d8236013c5db8960c44596
mckeeverkatherine <40339116+mckeeverkatherine@users.noreply.github.com>
2018-08-21 20:28:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('main.js is connected!');
+
+// /* <script type="text/javascript"> */
+// (function e(){var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="https://www.staticw2.yotpo.com/2SeuSMZdYkOryRWb9a6jqB28SkPmeC8xHeDFVweL/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
+// // </script>
